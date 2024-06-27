e5a96993746ba8c72de0ceffbfd508a5f7363acd
mhi1001 <35073349+mhi1001@users.noreply.github.com>
2024-06-27 22:39:44 +0200

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import joblib
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -8,18 +8,14 @@ app = Flask(__name__)
 
 # Load the trained Random Forest model
 # model = joblib.load('random_forest_model.pkl')
-model = joblib.load('diabetes_logistic_modelMINE.pkl')
-# Standard scaler used during model training
-scaler = joblib.load('scaler.pkl')
+model = joblib.load('diabetes_logistic_model.pkl')
+
 # Load the label encoders
 label_encoder_gender = LabelEncoder()
 label_encoder_gender.classes_ = np.load('label_encoder_gender_classes.npy', allow_pickle=True)
+
 label_encoder_diabetes = LabelEncoder()
 label_encoder_diabetes.classes_ = np.load('label_encoder_diabetes_classes.npy', allow_pickle=True)
-label_encoder_bmi = LabelEncoder()
-label_encoder_bmi.classes_ = np.load('label_encoder_bmi_classes.npy', allow_pickle=True)
-label_encoder_bp = LabelEncoder()
-label_encoder_bp.classes_ = np.load('label_encoder_bp_classes.npy', allow_pickle=True)
 
 # Define the home route
 @app.route('/')
@@ -32,32 +28,25 @@ def predict():
     # Get the form data
     data = request.form.to_dict()
     features = np.array([[
-        float(data['Cholesterol']),
         float(data['Glucose']),
-        float(data['HDL_Chol']),
-        float(data['Chol_HDL_ratio']),
         int(data['Age']),
-        int(label_encoder_gender.transform([data['Gender']])[0]),
-        float(data['Height']),
-        float(data['Weight']),
+        float(data['waist']),
+        float(data['Cholesterol']),
         float(data['BMI']),
         float(data['Systolic_BP']),
-        float(data['Diastolic_BP']),
-        float(data['waist']),
-        float(data['hip']),
-        float(data['Waist_hip_ratio']),
-        int(data['BMI_Category']),
-        int(data['BP_Category']),
-        float(data['Waist_to_Height'])
+        float(data['Diastolic_BP'])
     ]])
 
-    # Scale the features
-    features_scaled = scaler.transform(features)
+    print(f"DEBUG inputed features:{features}")
 
     # Make a prediction
-    prediction = model.predict(features_scaled)
-    probability = model.predict_proba(features_scaled)[0][0]  
-    prediction_text = 'Diabetes' if prediction[0] == 0 else 'No Diabetes'
+    prediction = model.predict(features)
+    #
+    # 0 - Diabetes
+    # 1 - No Diabetes
+    probability = model.predict_proba(features)[0][1]  
+    prediction_text = label_encoder_diabetes.inverse_transform(prediction)[0]
+    print(f"DEBUG prediction: {prediction[0]}")
     probability_text = f'Likelihood of having diabetes: {probability * 100:.2f}%'
 
     return render_template('index.html', prediction_text=prediction_text, probability_text=probability_text)
