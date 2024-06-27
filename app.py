579e96ed194864d050a56c85eef9a383e2244df3
mhi1001 <35073349+mhi1001@users.noreply.github.com>
2024-06-27 20:38:13 +0200

    old models/classes etc

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -10,6 +11,15 @@ app = Flask(__name__)
 model = joblib.load('diabetes_logistic_modelMINE.pkl')
 # Standard scaler used during model training
 scaler = joblib.load('scaler.pkl')
+# Load the label encoders
+label_encoder_gender = LabelEncoder()
+label_encoder_gender.classes_ = np.load('label_encoder_gender_classes.npy', allow_pickle=True)
+label_encoder_diabetes = LabelEncoder()
+label_encoder_diabetes.classes_ = np.load('label_encoder_diabetes_classes.npy', allow_pickle=True)
+label_encoder_bmi = LabelEncoder()
+label_encoder_bmi.classes_ = np.load('label_encoder_bmi_classes.npy', allow_pickle=True)
+label_encoder_bp = LabelEncoder()
+label_encoder_bp.classes_ = np.load('label_encoder_bp_classes.npy', allow_pickle=True)
 
 # Define the home route
 @app.route('/')
@@ -27,7 +37,7 @@ def predict():
         float(data['HDL_Chol']),
         float(data['Chol_HDL_ratio']),
         int(data['Age']),
-        int(data['Gender']),
+        int(label_encoder_gender.transform([data['Gender']])[0]),
         float(data['Height']),
         float(data['Weight']),
         float(data['BMI']),
@@ -46,9 +56,11 @@ def predict():
 
     # Make a prediction
     prediction = model.predict(features_scaled)
+    probability = model.predict_proba(features_scaled)[0][0]  
     prediction_text = 'Diabetes' if prediction[0] == 0 else 'No Diabetes'
+    probability_text = f'Likelihood of having diabetes: {probability * 100:.2f}%'
 
-    return render_template('index.html', prediction_text=f'Prediction: {prediction_text}')
+    return render_template('index.html', prediction_text=prediction_text, probability_text=probability_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
